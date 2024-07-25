23915ee6ecec17a6cf00dab37c4e92b783f4f958
Marco Ippolito <marcoippolito54@gmail.com>
2024-07-25 08:56:21 +0200

    chore: update swc wasm

--- lib/swc/wasm.d.ts
+++ lib/swc/wasm.d.ts
@@ -1,19 +1,27 @@
 /* tslint:disable */
 /* eslint-disable */
-/**
-* @param {string} input
-* @param {any} options
-* @returns {Promise<any>}
-*/
-export function transform(input: string, options: any): Promise<any>;
-/**
-* @param {string} input
-* @param {any} options
-* @returns {any}
-*/
-export function transformSync(input: string, options: any): any;
 
 export function transform(src: string, opts?: Options): Promise<TransformOutput>;
 export function transformSync(src: string, opts?: Options): TransformOutput;
+
+
+
+interface Options {
+    module?: boolean;
+    filename?: string;
+    mode?: Mode;
+    sourceMap?: boolean;
+}
+
+
+
+type Mode = "strip-only" | "transform";
+
+
+
+interface TransformOutput {
+    code: string;
+    map?: string;
+}
 
 
