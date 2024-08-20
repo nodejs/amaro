fe858c7c41a1f6cab56b89d9e1fb7660a5833310
Marco Ippolito <marcoippolito54@gmail.com>
2024-08-20 09:32:07 +0200

    feat: add transform loader

---
+++ esbuild.config.mjs
@@ -0,0 +1,44 @@
+import { build } from "esbuild";
+import { copy } from "esbuild-plugin-copy";
+
+const copyPlugin = copy({
+	assets: [
+		{
+			from: ["./src/register/register-strip.mjs"],
+			to: ["."],
+		},
+		{
+			from: ["./src/register/register-transform.mjs"],
+			to: ["."],
+		},
+		{
+			from: ["./lib/LICENSE", "./lib/package.json"],
+			to: ["."],
+		},
+	],
+});
+
+await build({
+	entryPoints: ["src/index.ts"],
+	bundle: true,
+	platform: "node",
+	target: "node22",
+	outfile: "dist/index.js",
+	plugins: [copyPlugin],
+});
+
+await build({
+	entryPoints: ["src/strip-loader.ts"],
+	bundle: false,
+	outfile: "dist/strip-loader.js",
+	platform: "node",
+	target: "node22",
+});
+
+await build({
+	entryPoints: ["src/transform-loader.ts"],
+	bundle: false,
+	outfile: "dist/transform-loader.js",
+	platform: "node",
+	target: "node22",
+});
