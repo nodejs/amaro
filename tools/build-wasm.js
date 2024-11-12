22351de91792cb10ba87f8cd736829a20f9640b8
Marco Ippolito <marcoippolito54@gmail.com>
2024-11-12 10:35:07 +0100

    Revert "fix: update to use common wasm builder"
    This reverts commit bb0e131de2c2c1bb5550f5970d71bf09d3721f9d.

--- tools/build-wasm.js
+++ tools/build-wasm.js
@@ -1,56 +1,29 @@
-const WASM_BUILDER_CONTAINER =
-	"ghcr.io/nodejs/wasm-builder@sha256:975f391d907e42a75b8c72eb77c782181e941608687d4d8694c3e9df415a0970"; // v0.0.9
-
-const { execSync, execFileSync } = require("node:child_process");
+const { execFileSync } = require("node:child_process");
 const { resolve } = require("node:path");
 
 const ROOT = resolve(__dirname, "../");
+const DOCKERFILE = resolve(__dirname, "./Dockerfile");
 
-let platform = process.env.WASM_PLATFORM;
-if (!platform && process.argv[2]) {
-	platform = execFileSync("docker", [
-		"info",
-		"-f",
-		"{{.OSType}}/{{.Architecture}}",
-	])
-		.toString()
-		.trim();
-}
+const name = `swc_build_wasm-${Date.now()}`;
 
-if (process.argv[2] === "--docker") {
-	const args = [];
-	args.push("run");
-	args.push("--rm");
-	args.push("--platform");
-	args.push(`${platform.toString().trim()}`);
-	if (process.platform === "linux") {
-		args.push("--user");
-		args.push(`${process.getuid()}:${process.getegid()}`);
-	}
-	args.push("--mount");
-	args.push(
-		`type=bind,source=${ROOT}/deps/swc/bindings,target=/home/node/build/bindings`,
-	);
-	args.push("--mount");
-	args.push(`type=bind,source=${ROOT}/lib,target=/home/node/build/lib`);
-	args.push("--mount");
-	args.push(`type=bind,source=${ROOT}/tools,target=/home/node/build/tools`);
-	args.push("--mount");
-	args.push(`type=bind,source=${ROOT}/deps,target=/home/node/build/deps`);
-	args.push("-t");
-	args.push(`${WASM_BUILDER_CONTAINER}`);
-	args.push("node");
-	args.push("./tools/build-wasm.js");
-	console.log(`> docker ${args}\n\n`);
-	execFileSync("docker", args, { stdio: "inherit" });
-	process.exit(0);
-}
+const buildArgs = [
+	"build",
+	"-t",
+	"swc_wasm_typescript",
+	"-f",
+	DOCKERFILE,
+	ROOT,
+];
+execFileSync("docker", buildArgs, { stdio: "inherit" });
 
-execSync(
-	`cd bindings/binding_typescript_wasm && \ 
-         cargo install --locked wasm-pack && \
-         PATH=/home/node/.cargo/bin:$PATH && \
-         ./scripts/build.sh && \
-         cp -r pkg/* ../../lib`,
-	{ stdio: "inherit" },
-);
+const runArgs = ["run", "-d", "--name", name, "swc_wasm_typescript"];
+execFileSync("docker", runArgs, { stdio: "inherit" });
+
+// Copies the new directory inside the Docker image to the host.
+const copyArgs = ["cp", `${name}:/usr/src/amaro/swc/.`, `${ROOT}/lib/`];
+execFileSync("docker", copyArgs, { stdio: "inherit" });
+
+// Removes the Docker image.
+execFileSync("docker", ["rm", name], { stdio: "inherit" });
+
+process.exit(0);
