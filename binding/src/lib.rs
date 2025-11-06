878f00b601d1631b2bc39ca6d014f2d0dfcfe40c
Donny/강동윤 <kdy1997.dev@gmail.com>
2025-11-06 15:55:26 +0900

    initialize

--- binding/src/lib.rs
+++ binding/src/lib.rs
@@ -1,8 +1,26 @@
 #![deny(clippy::all)]
 
 use napi_derive::napi;
+use swc_ts_fast_strip::Options;
+
+#[napi(object)]
+pub struct TransformOptions {}
+
+#[napi(object)]
+pub struct TransformOutput {
+  pub code: String,
+  pub map: Option<String>,
+}
 
 #[napi]
-pub fn plus_100(input: u32) -> u32 {
-  input + 100
+pub fn transform(code: String, options: TransformOptions) -> napi::Result<TransformOutput> {
+  let options = Options {};
+
+  let result = swc_ts_fast_strip_binding::transform(code, options)
+    .map_err(|e| napi::Error::from_reason(serde_json::to_string(&e[0]).unwrap()))?;
+
+  Ok(TransformOutput {
+    code: result.code,
+    map: result.map,
+  })
 }
