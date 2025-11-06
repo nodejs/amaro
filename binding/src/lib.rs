1915ec131b620ee83c707e9d59a0571d92d21524
Donny/강동윤 <kdy1997.dev@gmail.com>
2025-11-06 15:56:15 +0900

    binding

--- binding/src/lib.rs
+++ binding/src/lib.rs
@@ -4,17 +4,14 @@ use napi_derive::napi;
 use swc_ts_fast_strip::Options;
 
 #[napi(object)]
-pub struct TransformOptions {}
-
-#[napi(object)]
 pub struct TransformOutput {
   pub code: String,
   pub map: Option<String>,
 }
 
 #[napi]
-pub fn transform(code: String, options: TransformOptions) -> napi::Result<TransformOutput> {
-  let options = Options {};
+pub fn transform(code: String, options: String) -> napi::Result<TransformOutput> {
+  let options = serde_json::from_str::<Options>(&options).expect("failed to parse options");
 
   let result = swc_ts_fast_strip_binding::transform(code, options)
     .map_err(|e| napi::Error::from_reason(serde_json::to_string(&e[0]).unwrap()))?;
